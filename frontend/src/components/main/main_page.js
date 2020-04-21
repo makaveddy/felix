6b8c37265cd13506a2531ced0e1beb1f1839367b
EFlores <eddyflores@icloud.com>
2020-04-20 20:43:38 -0400

    set up frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Felix</h1>
+        <footer>Copyright &copy; 2020 Felix</footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
