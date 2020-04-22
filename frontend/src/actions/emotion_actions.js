869d82a226e0ba0e9bf0be8e777e36a8b761a187
karlfleener <karlfleener@gmail.com>
2020-04-22 12:06:25 -0400

    start media and emotion actions

--- frontend/src/actions/emotion_actions.js
+++ frontend/src/actions/emotion_actions.js
@@ -1,2 +1,16 @@
 import * as APIUtil from "../util/emotion_api_util";
 
+export const RECEIVE_EMOTIONS = "RECEIVE_EMOTIONS";
+export const RECEIVE_EMOTION = "RECEIVE_EMOTION";
+
+export const receiveEmotions = emotions => ({
+    type: RECEIVE_EMOTIONS,
+    emotions
+});
+
+export const receiveEmotion = emotion => ({
+    type: RECEIVE_EMOTION,
+    emotion
+});
+
+export const 
--- frontend/src/actions/media_actions.js
+++ frontend/src/actions/media_actions.js
@@ -1,2 +1,8 @@
 import * as APIUtil from "../util/media_api_util";
 
+export const RECEIVE_MEDIA = 'RECEIVE_MEDIA'
+
+export const receiveMedia = media => ({
+  type: RECEIVE_MEDIA,
+  media
+})
