23817ee89e6604648905c797f6cc9665273b9d94
Aimy <aimy1yu@gmail.com>
2020-05-19 12:52:03 -0400

    Remove comments

--- frontend/src/util/media_api_util.js
+++ frontend/src/util/media_api_util.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-// export const fetchAllMedia = () => {
-//   return axios.get("/api/emotions/media");
-// };
-
 export const fetchMedia = (mediaId) => {
   return axios.get(`/api/emotions/emotionId/${mediaId}`);
 };
